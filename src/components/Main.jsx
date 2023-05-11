5ab9c22576311a9c19837d0df2e676e87e14621c
mikkopaa <mikko.paajanen@metropolia.fi>
2023-05-11 19:01:14 +0300

    Added Routes

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,13 +1,17 @@
 import Constants from 'expo-constants';
 import { Text, StyleSheet, View, Pressable } from 'react-native';
+import { Route, Routes, Navigate } from 'react-router-native'
+
 import AppBar from './AppBar';
 import RepositoryList from './RepositoryList';
+import theme from '../theme';
+import SignIn from './SignIn';
 
 const styles = StyleSheet.create({
   container: {
     flexGrow: 1,
     flexShrink: 1,
-    backgroundColor: '#e1e4e8',
+    backgroundColor: theme.colors.mainBackground,
   },
 });
 
@@ -15,7 +19,11 @@ const Main = () => {
   return (
     <View style={styles.container}>
       <Pressable onPress={() => console.log('pressed..')}><AppBar /></Pressable>
-      <RepositoryList />
+      <Routes>
+        <Route path="/" element={<RepositoryList />} exact />
+        <Route path="/Signin" element={<SignIn />} exact />
+        <Route path="*" element={<Navigate to="/" replace />} />
+      </Routes>
     </View>
   );
 };
