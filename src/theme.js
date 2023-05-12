f27e1cb7d0efe2f966f998a355e49e21b95bb344
mikkopaa <mikko.paajanen@metropolia.fi>
2023-05-12 11:40:47 +0300

    Ex10.10 finished

--- src/theme.js
+++ src/theme.js
@@ -1,17 +1,24 @@
+import { Platform } from 'react-native'
+
 const theme = {
   colors: {
     textPrimary: '#24292e',
     textSecondary: '#586069',
     primary: '#0366d6',
     tabBackground: '#24292e',
-    tabText: '#eeeeee'
+    tabText: '#eeeeee',
+    mainBackground: '#e1e4e8'
   },
   fontSizes: {
     body: 14,
     subheading: 16,
   },
   fonts: {
-    main: 'System',
+    main: Platform.select({
+      android: 'Roboto',
+      ios: 'Arial',
+      default: 'System'
+    })
   },
   fontWeights: {
     normal: '400',
